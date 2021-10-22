e71d85cbbe983c50f543c5f7d3151e920912ed87
minusvetal <minusvetal@gmail.com>
2021-10-22 13:05:19 +0300

    add product

--- js/main.js
+++ js/main.js
@@ -9,13 +9,11 @@ const discount = new Swiper(".discount", {
 // categories
 
 const categories = new Swiper(".categories__container", {
-	// prevButton: ".categories__arrow-right",
 	slidesPerView: 6,
 	spaceBetween: 25,
 	grabCursor: true,
 	loop: true,
 	direction: "horizontal",
-	// nextButton: "arrow-left",
 	navigation: {
 		nextEl: ".categories__arrow-right",
 		prevEl: ".categories__arrow-left",
