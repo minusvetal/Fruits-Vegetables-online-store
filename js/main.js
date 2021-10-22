09979121d11926c4850aca7ca7b8ab20d4f68981
minusvetal <minusvetal@gmail.com>
2021-10-22 12:36:31 +0300

    add couresel categiries

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ const discount = new Swiper(".discount", {
 	grabCursor: true,
 	loop: true,
 });
+
+// categories
+
+const categories = new Swiper(".categories__container", {
+	// prevButton: ".categories__arrow-right",
+	slidesPerView: 6,
+	spaceBetween: 25,
+	grabCursor: true,
+	loop: true,
+	direction: "horizontal",
+	// nextButton: "arrow-left",
+	navigation: {
+		nextEl: ".categories__arrow-right",
+		prevEl: ".categories__arrow-left",
+	},
+});
