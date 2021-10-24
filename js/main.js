a0b14ce2c1d97462f84dc9f643cc2e8a087e4f06
minusvetal <minusvetal@gmail.com>
2021-10-24 14:20:36 +0300

    add + -  выбор товара и фавор jGuery переключатель

--- js/main.js
+++ js/main.js
@@ -49,3 +49,41 @@ $(document).ready(function () {
 		`,
 	});
 });
+
+// favorite
+$(".product-slider__favorite").on("click", function (e) {
+	e.preventDefault();
+	$(this).toggleClass("product-slider__favorite-active");
+});
+
+// кнопки плюс минус
+
+// Убавляем кол-во по клику
+$(".product-count .product-count__minus").click(function () {
+	let $input = $(this).parent().find(".quantity");
+	let count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+});
+// Прибавляем кол-во по клику
+$(".product-count .product-count__plus").click(function () {
+	let $input = $(this).parent().find(".quantity");
+	let count = parseInt($input.val()) + 1;
+	count =
+		count > parseInt($input.data("max-count"))
+			? parseInt($input.data("max-count"))
+			: count;
+	$input.val(parseInt(count));
+});
+// Убираем все лишнее и невозможное при изменении поля
+$(".product-count .quantity").bind("change keyup input click", function () {
+	if (this.value.match(/[^0-9]/g)) {
+		this.value = this.value.replace(/[^0-9]/g, "");
+	}
+	if (this.value == "") {
+		this.value = 1;
+	}
+	if (this.value > parseInt($(this).data("max-count"))) {
+		this.value = parseInt($(this).data("max-count"));
+	}
+});
